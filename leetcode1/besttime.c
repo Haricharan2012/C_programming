430fca5aa43ca19c87dfe665ca7a1618ac5aeb7e
Haricharan <Haricharan43019@gmail.com>
2025-11-13 15:03:02 +0530

    Thu Nov 13 03:03:02 PM IST 2025

--- leetcode1/besttime.c
+++ leetcode1/besttime.c
@@ -2,50 +2,68 @@
 int maxprofit(int* prices,int pricesize);
 
 int main(int argc,char *argv)
-  {
+{
   
-    int prices[]={7,6,4,3,1};
-    
-    int pricesSize=sizeof(prices)/sizeof(prices[0]);
+	int prices[]={5};
+	int pricesSize=sizeof(prices)/sizeof(prices[0]);
   
-    maxprofit(prices,pricesSize);
-  
-    
-    }
+    	maxprofit(prices,pricesSize);    
+}
     
     
 int maxprofit(int* prices,int pricesSize)
-  {
+{
   
    int smallest;
    
    smallest=prices[0];
-   
-    for(int i=1;i<pricesSize;i++)
+   int largest=0;	
+   int k=0;
+
+    for(int i=0;i<pricesSize;i++)
        {
          if(prices[i]<smallest)
             {
               smallest=prices[i];
               
-              printf("\n the samllest is %d",smallest);
+             
+              k=i+1;
               
-              int largest=i;
               
-              for(int k=i+1;k<pricesSize;k++)
-                 {
-                   if(prices[k]>largest)
-                     {
-                      largest=prices[k];
-                      } 
+            }
+         
+             //largest=prices[k+1];
+             
+        }
+        
+      for( k;k<pricesSize;k++)
+      
+           {
+              if(prices[k]>largest)
+                {
+                  largest=prices[k];
+                 }
+           } 
+      
                      
-                  }
+             //    }
+                  
+                  printf("\n the samllest is %d",smallest);
+             if(k>=pricesSize)
+              {
+               		 printf("\n failure");  //return null
+              } 
+             else
+                {
+                  printf("\n k= %d",k);
+                }
+                  
                   printf("\n largest is %d",largest);
+                  printf("\n size %d",pricesSize);
                  int  profit=largest-smallest;
                   printf("\n profit is %d",profit);
                  }
-                 
-               
-  
-  }
- }	
+             
+  	
+	
     
