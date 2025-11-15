192229f653316cad5b20f82b6f034d9d71f516bd
Haricharan <Haricharan43019@gmail.com>
2025-11-15 21:56:51 +0530

    Sat Nov 15 09:56:51 PM IST 2025

--- leetcode1/besttime.c
+++ leetcode1/besttime.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 int maxprofit(int* prices,int pricesize);
+int profit=0;
 
 int main(int argc,char *argv)
 {
   
-	int prices[]={5};
+	int prices[]={4};
 	int pricesSize=sizeof(prices)/sizeof(prices[0]);
   
-    	maxprofit(prices,pricesSize);    
+    	maxprofit(prices,pricesSize);   
+    	
+    	printf("\n profit is %d",profit); 
+    	
+    
 }
     
     
@@ -25,44 +30,44 @@ int maxprofit(int* prices,int pricesSize)
          if(prices[i]<smallest)
             {
               smallest=prices[i];
-              
-             
               k=i+1;
-              
-              
             }
-         
-             //largest=prices[k+1];
-             
         }
         
-      for( k;k<pricesSize;k++)
-      
-           {
-              if(prices[k]>largest)
-                {
-                  largest=prices[k];
-                 }
+       
+      for(k;k<pricesSize;k++)
+           {	 
+                 if(prices[k]>largest)
+                 
+                   {
+                   
+                    largest=prices[k];
+          
+                    }
+                
            } 
-      
-                     
-             //    }
+           
+     
+             
+             if(largest>0 && largest!=smallest)
+             {
                   
-                  printf("\n the samllest is %d",smallest);
-             if(k>=pricesSize)
-              {
-               		 printf("\n failure");  //return null
-              } 
-             else
-                {
-                  printf("\n k= %d",k);
-                }
-                  
-                  printf("\n largest is %d",largest);
-                  printf("\n size %d",pricesSize);
-                 int  profit=largest-smallest;
-                  printf("\n profit is %d",profit);
+              printf("\n the samllest is %d",smallest);
+              printf("\n largest is %d",largest);
+              profit=largest-smallest;
+              
+              return profit;
+             }
+
+   
+               
+              else
+                 {
+                  printf("\n failed not possible");
+                  return profit;
                  }
+    
+}
              
   	
 	
