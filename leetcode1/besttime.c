5a3788a2b366866a881d06f6a6dfcab2ddb33e47
Haricharan <Haricharan43019@gmail.com>
2025-11-16 15:03:01 +0530

    Sun Nov 16 03:03:01 PM IST 2025

--- leetcode1/besttime.c
+++ leetcode1/besttime.c
@@ -5,7 +5,7 @@ int profit=0;
 int main(int argc,char *argv)
 {
   
-	int prices[]={4};
+	int prices[]={7,1,5,3,6,4};
 	int pricesSize=sizeof(prices)/sizeof(prices[0]);
   
     	maxprofit(prices,pricesSize);   
@@ -19,7 +19,8 @@ int main(int argc,char *argv)
 int maxprofit(int* prices,int pricesSize)
 {
   
-   int smallest;
+   int smallest;      //smallest value
+   int secsmallest;   //second smallestvalue
    
    smallest=prices[0];
    int largest=0;	
@@ -27,27 +28,54 @@ int maxprofit(int* prices,int pricesSize)
 
     for(int i=0;i<pricesSize;i++)
        {
-         if(prices[i]<smallest)
+         if(prices[i]<smallest)      //if any element is less than the smallest value
             {
-              smallest=prices[i];
-              k=i+1;
+              smallest=prices[i];     
+              k=i+1;                //find largest from the next index after smallest val
             }
         }
         
        
-      for(k;k<pricesSize;k++)
+        
+       
+      for(k;k<pricesSize;k++)                        	
            {	 
-                 if(prices[k]>largest)
+                 if(prices[k]>largest)    // if any elemnt is greater than lasgest element 
                  
                    {
                    
                     largest=prices[k];
+                    
           
                     }
                 
            } 
            
-     
+           
+             if(smallest==0)                //if it is found out that smallest is zero then 
+                { 
+                	//k=0;
+                
+                  for(int t=0;t<pricesSize;t++)
+                     {
+                        if(prices[t]>0)      // if element is greater than zero but less than the largest value
+                          {
+                          
+                          
+                            secsmallest=prices[t];                     //find the second smallest number
+                            smallest=secsmallest;
+                           
+                          //  k=t+1;
+                           }
+                     }
+                  }
+           
+              printf("\n second %d",secsmallest);
+              printf("\n the samllest is %d",smallest);
+              printf("\n largest is %d",largest);
+           
+            
+                       
              
              if(largest>0 && largest!=smallest)
              {
@@ -60,7 +88,7 @@ int maxprofit(int* prices,int pricesSize)
              }
 
    
-               
+       
               else
                  {
                   printf("\n failed not possible");
