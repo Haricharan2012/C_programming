ab4aa061ee8fac0e2dd1f44c4c5c289e073034f3
Haricharan <Haricharan43019@gmail.com>
2025-11-18 15:03:01 +0530

    Tue Nov 18 03:03:01 PM IST 2025

--- leetcode1/conduplicate.c
+++ leetcode1/conduplicate.c
@@ -1,16 +1,34 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 int duplicate(int* nums,int numsize);
+
+// Comparator to sort in ascending order
+int comp(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
 
 int main(int argc,char *argv[])
    {
-     int nums[]={1,2,3,1};
+     int nums[]={1,1,1,3,3,4,3,2,4,2};
      int numsize=sizeof(nums)/sizeof(nums[0]);
+     qsort(nums,numsize,sizeof(nums[0]),comp);
+     
+     for(int i=0;i<numsize;i++)
+        {
+         printf("\n %d",nums[i]);
+        }
+        
      duplicate(nums,numsize);
+     
     }
     
 int duplicate(int* nums,int numsize)
     {
     
+     bool flag=false;
+     
       for(int i=0;i<numsize;i++)
           {
          //   for(int j=i+1;j<numsize;j++)
@@ -19,11 +37,22 @@ int duplicate(int* nums,int numsize)
                 if(nums[i]==nums[i+1])
                   {
                     printf("\n exists for %d at %d",nums[i],i+1);
+                    
+                    flag=true;
                   }
-                else
+                  
+                  if(flag)
+                   {
+                     return true;
+                   }
+                  else
+                    {
+                     return false;
+                    }
+               /* else
                   {
                     printf("\n does not exist for %d",nums[i]);
-                  }
+                  } */
                //}
            }
            
