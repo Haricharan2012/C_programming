d218d76b22426daa787a01cb5d43bd859d966b1a
Haricharan <Haricharan43019@gmail.com>
2025-11-27 15:03:02 +0530

    Thu Nov 27 03:03:02 PM IST 2025

--- leetcode1/maxsubarr.c
+++ leetcode1/maxsubarr.c
@@ -1,37 +1,49 @@
+
+/*************name**************************************
+*************start date********************************
+*************modified date*****************************
+*************description*******************************/
+
 #include<stdio.h>
 int maxSubArray(int* nums,int numsSize);
 
 int main(int argc,char * argv[])
- {
-    
-   int nums[]={-2,1,-3,4,-1,2,1,-5,4};
-   int numsSize=sizeof(nums)/sizeof(nums[0]);
+{    
+	int nums[]={-2,1,-3,4,-1,2,1,-5,4};
+	int numsSize=sizeof(nums)/sizeof(nums[0]);
    
-   maxSubArray(nums,numsSize);
-
- }
-
-
+	maxSubArray(nums,numsSize);
+}
 
 int maxSubArray(int* nums,int numsSize)
-  {
+{
   
-  int sum=0;
-  int max=0;
+	int sum=0;
+  	int max=0;
+  	int ct=0;
   
-  for(int i=0;i<numsSize;i++)
-     {
-       sum=sum+nums[i];         
-    
-     }
+  	loop:
+  	if (ct == numsSize) 
+   	{
+    		return 0; 
+   	}
+  
+  	sum=0;
+  	
+  	for(int i = ct; i <numsSize; i++)
+     	{
+       		sum = sum + nums[i];         
+       		printf("\n sum is %d",sum);
+     	}
+     	ct=ct+1;
      
-     printf("\n the sum is %d",sum);
-   
-     if(sum>max)
-       {
-        max=sum;
-       }
-      printf("\n the max of the array is %d",max);
- 
-   }
+      	if(sum>max)
+       	{
+        	max=sum;
+       	}
+      	printf("\n the max of the array is %d",max);      
+     	goto loop;   
+     	printf("\n the sum is %d",sum);
+    
+}
 
