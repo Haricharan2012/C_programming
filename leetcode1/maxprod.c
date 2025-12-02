b24bdbbacdc76ce42bae23fb984144800ede9746
Haricharan <Haricharan43019@gmail.com>
2025-12-02 13:12:03 +0530

    Tue Dec  2 01:12:03 PM IST 2025

---
+++ leetcode1/maxprod.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int prodmax(int *nums,int numsSize);
+
+int main(int argc,char *argv[])
+{
+
+int nums[]={-3,-1,-1};
+int numsSize= sizeof(nums)/sizeof(nums[0]);
+int result=prodmax(nums,numsSize);
+printf("\n the product od the array is %d",result);
+}
+
+int prodmax(int* nums, int numsSize) {
+
+    int maxprod=nums[0];
+    int currprod=0;
+
+    //applying kadane's algorithm
+
+    for(int i=0;i<numsSize;i++)
+    {
+        if(currprod==0)    //if prod is non negative
+        {
+            currprod=nums[i];  //currprod value updated
+        }
+        else
+        {
+            currprod=currprod*nums[i];   //currprod value reset to current 
+        }
+
+        if(currprod>maxprod)
+        {
+            maxprod=currprod;  //maxprod value updated if currprod is greater else remains default
+        }
+    }
+
+    return maxprod;
+    
+}
+
