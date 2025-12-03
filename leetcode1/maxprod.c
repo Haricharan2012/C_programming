9a91c60acd4ac187b151e7e44240390a963419d3
Haricharan <Haricharan43019@gmail.com>
2025-12-03 15:03:01 +0530

    Wed Dec  3 03:03:01 PM IST 2025

--- leetcode1/maxprod.c
+++ leetcode1/maxprod.c
@@ -5,22 +5,39 @@ int prodmax(int *nums,int numsSize);
 int main(int argc,char *argv[])
 {
 
-int nums[]={-3,-1,-1};
-int numsSize= sizeof(nums)/sizeof(nums[0]);
-int result=prodmax(nums,numsSize);
-printf("\n the product od the array is %d",result);
+	int nums[]={3,-1,4};
+	int numsSize= sizeof(nums)/sizeof(nums[0]);
+		prodmax(nums,numsSize);
+	//printf("\n the product od the array is %d",result);
 }
 
-int prodmax(int* nums, int numsSize) {
+int prodmax(int* nums, int numsSize) 
+{
 
-    int maxprod=nums[0];
-    int currprod=0;
+	int maxprod=nums[0];
+	int currprod=0;
 
     //applying kadane's algorithm
-
-    for(int i=0;i<numsSize;i++)
-    {
-        if(currprod==0)    //if prod is non negative
+	for(int i=0;i<numsSize;i++)
+	{
+	        currprod=1;
+	    	for(int j=i;j<numsSize;j++)
+    		{
+    		
+    			currprod=currprod*nums[j];
+    			
+    			if(currprod > maxprod)
+    			{
+            			maxprod = currprod; 
+            	        }
+    		}
+    		 
+    	  }
+    		
+    		printf("\n the maximum product is %d",maxprod);
+}
+    		
+    	        /*if(currprod==0)    //if prod is non negative
         {
             currprod=nums[i];  //currprod value updated
         }
@@ -32,10 +49,10 @@ int prodmax(int* nums, int numsSize) {
         if(currprod>maxprod)
         {
             maxprod=currprod;  //maxprod value updated if currprod is greater else remains default
-        }
-    }
+        } */
+  //  }
 
-    return maxprod;
+  //  return maxprod;
     
-}
+//}
 
