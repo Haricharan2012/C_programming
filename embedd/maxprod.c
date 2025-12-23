b140c99de0ae9c242b7f0761bb802beb6621f5fe
Haricharan <Haricharan43019@gmail.com>
2025-12-23 22:03:00 +0530

    Tue Dec 23 10:03:00 PM IST 2025

---
+++ embedd/maxprod.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+int prodmax(int *nums,int numsSize);
+
+int main(int argc,char *argv[])
+{
+
+	int nums[]={3,-1,4};
+	int numsSize= sizeof(nums)/sizeof(nums[0]);
+		prodmax(nums,numsSize);
+	//printf("\n the product od the array is %d",result);
+}
+
+int prodmax(int* nums, int numsSize) 
+{
+
+	int maxprod=nums[0];
+	int currprod=0;
+
+    
+	for(int i=0;i<numsSize;i++)
+	{
+	        currprod=1;
+	    	for(int j=i;j<numsSize;j++)
+    		{
+    		
+    			currprod=currprod*nums[j];
+    			
+    			if(currprod > maxprod)
+    			{
+            			maxprod = currprod; 
+            	        }
+    		}
+    		 
+    	  }
+    		
+    		printf("\n the maximum product is %d",maxprod);
+}
+    		
+    	        /*if(currprod==0)    //if prod is non negative
+        {
+            currprod=nums[i];  //currprod value updated
+        }
+        else
+        {
+            currprod=currprod*nums[i];   //currprod value reset to current 
+        }
+
+        if(currprod>maxprod)
+        {
+            maxprod=currprod;  //maxprod value updated if currprod is greater else remains default
+        } */
+  //  }
+
+  //  return maxprod;
+    
+//}
+
