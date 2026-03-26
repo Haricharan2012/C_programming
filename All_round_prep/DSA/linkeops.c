08b8d1d4a6547f59927dfdd564a1c30d0411d754
Haricharan <Haricharan43019@gmail.com>
2026-03-26 15:03:02 +0530

    Thu Mar 26 03:03:02 PM IST 2026

--- All_round_prep/DSA/linkeops.c
+++ All_round_prep/DSA/linkeops.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 struct node
 {
 	int data;
 	struct node* next;
-}
+};
+
+void insert_beg(struct node **head,int data);
+void insert_mid(struct node *prev,int data);
+void insert_end(struct node **head,int data);
+void deletenode(struct node **head,int key);
+int searchnode(struct node *head,int key);
+void sortlist(struct node **head);
+void printlist(struct node *node);
+
+
+
 
 int main()
 {
@@ -20,6 +31,7 @@ int main()
 	
 	//print
 	printf("\n the linked list is");
+	printf("\n");
 	printlist(head);
 	
 	//deletion
@@ -27,53 +39,57 @@ int main()
 	deletenode(&head,3);
 	//print
 	printf("\n after deletion");
+	printf("\n");
 	printlist(head);
 	
 	
 	//search
 	int find=5;
 	
-	if(searchnode(&head,find))
+	if (searchnode(head,find))
 	{
 		printf("\n item exists");
+		printf("\n");
 	}
 	else
 	{
 		printf("\n item dosen't exists");
+		printf("\n");
 	}
 	
 	//sorting
 	
 	sortlist(&head);
 	printf("\n the sorted list is");
+	printf("\n");
 	printlist(head);
 }
 
 //---------------------Insertion---------------------------------------------------------------------//
 
 //---------------------Insert_Beg---------------------------------------------------//
-void insert_beg(struct node** head,int data)
+void insert_beg(struct node **head,int data)
 {
 	//allocate memmory
-	struct node* newnode= struct node* malloc(sizeof(struct node));
+	struct node* newnode= (struct node*) malloc(sizeof(struct node));
 	newnode->data=data;
 	newnode->next=*head; //new node points to first element
 	*head=newnode; //head pointer points to newnode
 }
 
 //---------------------Insert_Mid--------------------------------------------------//
-void insert_mid(struct node* prev,int data)
+void insert_mid(struct node *prev,int data)
 {
 	if(prev==NULL)
 	{
 		printf("\n prev ele can't be null");
 		return;
 	}
-	
+		
 	else
 	{
 		//allocate memory
-		struct node* newnode=struct node* malloc(sizeof(struct node));
+		struct node* newnode=(struct node*) malloc(sizeof(struct node));
 		newnode->data=data;
 		newnode->next=prev->next; //new node points to whatever previous node was pointing to
 		prev->next=newnode; //previous node points to new node
@@ -82,16 +98,16 @@ void insert_mid(struct node* prev,int data)
 
 //---------------------Insert_end-------------------------------------------------//
 
-void insert_end(struct node** head,int data)
+void insert_end(struct node **head,int data)
 {
 	//ALLOCATE MEMORY
-	struct node* newnode=struct node* malloc(sizeof(struct node));
+	struct node* newnode=(struct node*) malloc(sizeof(struct node));
 	struct node* last=*head; //start from beginning
 	
 	newnode->data=data;
 	newnode->next=NULL; //supposed to be last node
 	
-	while(last!=NULL)
+	while(last->next!=NULL)
 	{
 		last=last->next;
 	}
@@ -104,7 +120,7 @@ void insert_end(struct node** head,int data)
 void deletenode(struct node **head,int key)
 {
 	struct node* temp=*head; 
-	struct node* prev;
+	struct node* prev=NULL;
 	
 	//if found in head//
 	if(temp!=NULL && temp->data==key)
@@ -135,15 +151,15 @@ void deletenode(struct node **head,int key)
 }
 
 //----------------search node-----------------------//
-void search(struct node** head,int key)
+int searchnode	(struct node *head,int key)
 {
-	struct node* current=head //start from head
+	struct node *current=  head; //start from head
 	
 	while(current != NULL)
 	{
 		if(current->data==key)  //if element matches with key 
 		{
-			return1;
+			return 1;
 		}
 		current=current->next;  //traverse through other elements
 	}
@@ -153,14 +169,14 @@ void search(struct node** head,int key)
 
 //------------------sortlist------------------------//
 
-void sortlist(struct node* head)
+void sortlist(struct node **head)
 {
-	struct node* current=head; //start from head
-	struct node* index=NULL; //nxt element 
+	struct node *current=*head; //start from head
+	struct node *index=NULL; //nxt element 
 	
 	int temp;
 	
-	if(head==NULL)
+	if(*head==NULL)
 	{
 		return;
 	}
@@ -168,7 +184,7 @@ void sortlist(struct node* head)
 	{
 		while(current!=NULL)
 		{
-			index=current->next //points to next element 
+			index=current->next;//points to next element 
 			
 			while(index!=NULL)
 			{
@@ -187,13 +203,14 @@ void sortlist(struct node* head)
 
 //----------------printlist------------------------//
 
-void printlist(struct node* node)
+void printlist(struct node *node)
 {
 	while(node!=NULL)
 	{
-		printf("%d",node->data);
+		printf("%d -> ",node->data);
 		node=node->next;
 	}
+	 printf("NULL");
 }
 	
 	
