186989ea374a68a95fddae3dd363ea62af2be6a6
Haricharan <Haricharan43019@gmail.com>
2026-04-09 15:03:01 +0530

    Thu Apr  9 03:03:01 PM IST 2026

--- basic/doubstr.c
+++ basic/doubstr.c
@@ -1,26 +1,27 @@
 #include<stdio.h>
+#include<math.h>
 
 int main()
 {
 	double num;
 	char str[30];
-	int rem=0.0;
+	//int rem=0.0;
 	
 	printf("\n enter a number to convert: ");
 	scanf("%lf",&num);
 	
-	double tmp=num;
-	int count=0;
+	double intp,floatp;
 	
-	while(tmp!=0.0)
-	{
-		rem=tmp%10;
-		tmp=tmp/10;
-		str[count]=rem+'\0';
-		count=count+1;
-	}
+	floatp= modf(num,&intp);
 	
-	str[count] = '\0';
+	printf("\n the floatpart is %f",floatp);
 	
-	printf("\n the current string is %s",str);
+	intp=num-floatp;
+	
+	printf("\n the int part is %f",intp);
+	
+	int newv=floor(intp);
+	
+	printf("\n the int part is %d",intp);
+
 }
