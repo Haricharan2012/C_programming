dcdf8f424a0fdbb3fa6d50bebd05115c34818bb0
Haricharan <Haricharan43019@gmail.com>
2026-04-21 15:03:02 +0530

    Tue Apr 21 03:03:02 PM IST 2026

--- All_round_prep/DSA/queue2.c
+++ All_round_prep/DSA/queue2.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 #define SIZE 5
 
+void dequeue();
+void enqueue();
+void disp();
+
+int items[SIZE];
+int front=-1;
+int rear=-1;
+
 int main()
 {
-	int items[SIZE]
-	int front=-1;
-	int rear=-1;
+	
 	int choice;
 	
 	while(1)
@@ -15,72 +21,76 @@ int main()
 		printf("\n 2. deletion  (dequeue)");
 		printf("\n 3. display");
 		
-		printf("\n enter your choice!");
-		scanf("%d",choice);
+		printf("\n enter your choice! ");
+		scanf("%d",&choice);
 		
 		switch(choice)
 		{
 			case 1: printf("\n -----------Enqueue------------");
-			
-			enqueue()
-			{
-				int val;
-				
-				printf("\n enter the value to insert in queue");
-				scanf("%d",&val);
-				
-				if(rear==size-1)
-				{
-					printf("\n queue is full");
-				}
-				else
-				{
-					front=0;
-					rear=rear+1;
-					items[rear]=val;
-					printf("\n value inserted");
-				}
-			}
+			enqueue();
 			break;
 			
-			case 2: printf("-------------dequeue--------------");
+			case 2: printf("\n-------------dequeue--------------");
+			dequeue();
+			break;
 			
-			dequeue()
-			{
-				if(front==-1)
-				{
-					printf("\n queue is empty");
-				}
-				else
-				{
-					front=front+1;
+			case 3: printf("\n--------------display-----------------\n");
+			disp();
+			break;
+		}
+	}
+}
+
+	void enqueue()
+	{
+		int val;
+				
+		printf("\n enter the value to insert in queue ");
+		scanf("%d",&val);
+				
+		if(rear==SIZE-1)
+		{
+			printf("\n queue is full");
+		}
+		else
+		{
+			front=0;
+			rear=rear+1;
+			items[rear]=val;
+			printf("\n value inserted");
+		}
+	}
+	
+	void dequeue()
+	{
+		if(front==-1)
+		{
+			printf("\n queue is empty");
+		}
+		else
+		{
+			front=front+1;
 					
-					if(front>rear)
-					{
-						front=rear=-1;
-					}
-				}
-			}
-			break;
-			
-			case 3: printf("--------------display-----------------");
-			
-			disp()
+			if(front>rear)
 			{
-				if(rear==-1)
-				{
-					printf("\n queue is empty");
-				}
-				else
-				{
-					for(int i=front;i<=rear;i++)
-					{
-						printf("%d",items[i]);
-					}
-					printf("\n");
-				}
+				front=rear=-1;
 			}
-			break;
+		}
+	}
+	
+	void disp()
+	{
+		if(rear==-1)
+		{
+			printf("\n queue is empty");
+		}
+		else
+		{
+			for(int i=front;i<=rear;i++)
+			{
+				printf("|%d|",items[i]);
+			}
+			printf("\n");
 		}
 	}
 						
