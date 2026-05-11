63e2fad235e8f55d1b9f6b310162953855a3e708
Haricharan <Haricharan43019@gmail.com>
2026-05-11 15:03:01 +0530

    Mon May 11 03:03:01 PM IST 2026

---
+++ All_round_prep/DSA/priqu.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void sawp(int *el1, int *el2);
+void insertion(int arr[],int sz,int i);
+void deletion(int arr[],int num);
+void disp(int arr[],int size);
+
+void main()
+{
+	int array[10];
+	printf("\n--------Priority_Queue--------");
+	printf("\n1.------insertion-------------");
+	printf("\n2.------Deletion--------------");
+	printf("\n3.------display---------------");
+	
+	
+
